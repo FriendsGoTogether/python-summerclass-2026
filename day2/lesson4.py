04f339c2f5d6f3eb7f70f2a3ccfb40affdde5cf8
Thu Cao <cao.trung.thu@gmail.com>
2026-06-07 13:13:54 +0700

    update code lesson 4

--- day2/lesson4.py
+++ day2/lesson4.py
@@ -14,3 +14,33 @@ school_name = "Vinschool SmartCity"
 birthday = "11/12/2017"
 grade = 9
 Is_excellent_student = True
+
+tuoi = 16 
+so_luong_hoc_sinh = 45 
+nhiet_do_sapa = -5
+
+diem_toan = 9.5 
+chieu_cao = 1.65
+so_pi = 3.14159
+
+ten = "Nguyễn Văn An"
+tin_nhan = 'Chúc bạn một ngày tốt lành!'
+so_dien_thoai = "0948314056"
+
+da_tot_nghiep = True
+da_an_sang = False
+
+trai_cay = ["Táo", "Chuối", "Cam", "Xoài"]
+diem_so = [8, 9.5, 7, 10]
+
+Age = int("15")
+
+str_age = "15"
+Age = int(str_age)
+
+decimal_number = float(34)
+decimal_number = float("3.14")
+
+str = int(34)
+str = int(34.14)
+
